c1d46b4c00a451cfdaf27814a68f76528b12973e
hadrien le bret <hadrien.lebret@gmail.com>
2020-11-11 16:49:09 +0100

    review

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,7 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :category, inclusion: { in: %w(chinese italian japanese french belgian),
     message: "%{value} is not a valid category" }
+  validates :name, presence: true
+  validates :address, presence: true
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :rating, numericality: { only_integer: true, less_than_or_equal_to: 5 }
+  validates :rating, numericality: { only_integer: true }, acceptance: { accept: [0, 1, 2, 3, 4, 5] }
   validates :content, presence: true
-
 end
