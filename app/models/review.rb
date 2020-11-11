2efe3df01e988bbcee012cbca126668f0f21f927
hadrien le bret <hadrien.lebret@gmail.com>
2020-11-11 14:23:50 +0100

    generating controller

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,4 @@
 class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :rating, numericality: { only_integer: true, less_than_or_equal_to: 5 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# db/seeds.rb
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+nems_nems = { name: "Nems Nems", address: "7 Boundary St, Pekin E2 7JE", category: "chinese" }
+pizza_napoli =  { name: "Pizza Napoli", address: "56A Marcel High St, Rome E1 6PQ", category: "italian" }
+croque_mme =  { name: "Croque Madame", address: "15 A Tommy High St, Paris E1 6PQ", category: "french" }
+sushi_mania =  { name: "Sushi Mania", address: "18 Richard High St, Tokyo E1 6PQ", category: "japanese" }
+frites =  { name: "Frites", address: "34 Megan High St, Bruxelles E1 6PQ", category: "belgian" }
+
+[ nems_nems, pizza_napoli, croque_mme, sushi_mania, frites ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
